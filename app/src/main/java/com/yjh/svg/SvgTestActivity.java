5fe6ab415bc680cd9d051112e57e01013979cb9e
jinhua <jinhua.yang@BKJK.COM>
2018-07-23 18:08:06 +0800

    svg test

--- app/src/main/java/com/yjh/svg/SvgTestActivity.java
+++ app/src/main/java/com/yjh/svg/SvgTestActivity.java
@@ -1,9 +1,13 @@
 package com.yjh.svg;
 
 import android.graphics.drawable.Animatable;
+import android.graphics.drawable.AnimatedVectorDrawable;
 import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -13,16 +17,28 @@ public class SvgTestActivity extends AppCompatActivity {
 
 
     ImageView imageView;
+    TextView textView;
+    AnimatedVectorDrawable drawable;
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_svg_test);
         imageView = findViewById(R.id.iv_svg_test);
-        Drawable drawable = imageView.getDrawable();
-        if(drawable instanceof Animatable){
-            ((Animatable)drawable).start();
-        }
+        textView = findViewById(R.id.tv_svg_start);
+        drawable = (AnimatedVectorDrawable)imageView.getResources().getDrawable(R.drawable.anim_vector);
+        imageView.setImageDrawable(drawable);
 
+        setListener();
+    }
+
+    public void setListener(){
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawable.start();
+            }
+        });
     }
 }
